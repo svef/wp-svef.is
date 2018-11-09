6ad028655d928928027c423e2d735ad029a71e2f
JonTryggvi <jontryggvi@jontryggvi.is>
2018-11-09 12:26:52 +0100

    partials function setup

--- functions.php
+++ functions.php
@@ -51,5 +51,8 @@ require_once( 'library/sticky-posts.php' );
 /** Configure responsive image sizes */
 require_once( 'library/responsive-images.php' );
 
+/** SVEF custom functions */
+require_once('library/svef/custom_functions.php');
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
---
+++ library/svef/custom_functions.php
@@ -0,0 +1,16 @@
+<?php
+	function svef_partial($path, $args = [], $echo = true) {
+		if (!empty($args)) {
+			extract($args);
+		}
+		if ($echo) {
+			include(locate_template($path . '.php'));
+			return;
+		}
+		ob_start();
+		include(locate_template($path . '.php'));
+		return ob_get_clean();
+	}
+
+
+?>
