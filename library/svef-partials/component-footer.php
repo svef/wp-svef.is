a6d75e4722f7bb32d6e08348488ad86708d2f0da
Birna Bryndís <thorkelsdottir@gmail.com>
2018-11-09 14:31:18 +0100

    sma footerstuff

---
+++ library/svef-partials/component-footer.php
@@ -0,0 +1,32 @@
+<footer class="footer section--dark">
+    <div class="footer-container grid-x">
+        <div class="footer__info cell small-6">
+            <div class="footer__intro">
+             <?php svef_partial("library/svef/icons/sveflogo.svg"); ?>
+                <p class="text--small">Samtök vefiðnaðarins (SVEF) eru fagsamtök þeirra er starfa að vefmálum á Íslandi. Samtökin hafa það að markmiði að miðla þekkingu og efla fagleg vinnubrögð í greininni.</p>
+            </div>
+            <div class="grid-x" >
+                <div class="cell small-6">
+                    <p>link</p>
+                    <p>link</p>
+                    <p>link</p>
+                    <p>link</p>
+                </div>
+                <div class="cell small-6">
+                    <div>
+                        <p>link</p>
+                        <p>link</p>
+                    </div>
+                    <div>
+                        <p>link</p>
+                        <p>link</p>
+                        <p>link</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__newsletter cell small-6">
+            <p>Newsletter here</p>
+        </div>
+    </div>
+</footer>
