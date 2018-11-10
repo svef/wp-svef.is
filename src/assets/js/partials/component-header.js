a673dc4c91d6f24173694e89bb6d560fd6abbb18
JonTryggvi <jontryggvi@jontryggvi.is>
2018-11-10 21:56:33 +0100

    Changed sass file structure so it is a little bit more clear, finalized menu click for component-header.js, added a global-functions.js

--- src/assets/js/partials/component-header.js
+++ src/assets/js/partials/component-header.js
@@ -1,3 +1,4 @@
+import GlobalFunctions from '../global-functions'
 const Header = {
   init() {
     this.cacheDom()
@@ -11,24 +12,37 @@ const Header = {
   },
   addEvents() {
     this.btnMenu.addEventListener('click', this.handleMenuClick.bind(this))
+    this.body.addEventListener('click', this.clickedOutSideMenu.bind(this))
+    this.body.addEventListener('keyup', this.escapeMenu.bind(this))
     window.addEventListener('scroll', this.windowScrollHandler.bind(this))
   },
   handleMenuClick(e) {
     e.preventDefault()
     if(this.btnMenu.classList.contains('nav__menu-button--clicked')) {
-        this.btnMenu.classList.remove('nav__menu-button--clicked')
-        this.sideMenu.classList.remove('side-menu--active')
-      } else {
-        this.btnMenu.classList.add('nav__menu-button--clicked')
-        this.sideMenu.classList.add('side-menu--active')
+      this.btnMenu.classList.remove('nav__menu-button--clicked')
+      this.sideMenu.classList.remove('side-menu--active')
+    } else {
+      this.btnMenu.classList.add('nav__menu-button--clicked')
+      this.sideMenu.classList.add('side-menu--active')
     }
   },
   windowScrollHandler(e) {
-
     if (e.target.scrollingElement.scrollTop > 160) {
       this.header.classList.add('site-header--shadow')
     } else {
       this.header.classList.remove('site-header--shadow')
+    }
+  },
+  clickedOutSideMenu(e) {
+    if (e.target != this.btnMenu && this.sideMenu.classList.contains('side-menu--active') && !this.sideMenu.contains(e.target) && !GlobalFunctions.isDescendant(this.sideMenu, e.target)) {
+      this.sideMenu.classList.remove('side-menu--active')
+      this.btnMenu.classList.remove('nav__menu-button--clicked')
+    }
+  },
+  escapeMenu(e) {
+    if (this.sideMenu.classList.contains('side-menu--active') && e.code == 'Escape') {
+      this.sideMenu.classList.remove('side-menu--active')
+      this.btnMenu.classList.remove('nav__menu-button--clicked')
     }
   }
 }
