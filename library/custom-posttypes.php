072415fcb7eb44acb19a5edc792c224f2c241e85
JonTryggvi <jontryggvi@jontryggvi.is>
2018-11-15 16:01:53 +0100

    fixed

--- library/custom-posttypes.php
+++ library/custom-posttypes.php
@@ -3,35 +3,55 @@
 		// creating (registering) the custom type
 		register_post_type( 'events', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
 			// let's now add all the options for this post type
-			array( 'labels' => array(
-				'name' => __( 'Events', 'foundationpress' ), /* This is the Title of the Group */
-				'singular_name' => __( 'Event', 'foundationpress' ), /* This is the individual type */
-				'all_items' => __( 'All Events', 'foundationpress' ), /* the all items menu item */
-				'add_new' => __( 'Add New', 'foundationpress' ), /* The add new menu item */
-				'add_new_item' => __( 'Add New Event', 'foundationpress' ), /* Add New Display Title */
-				'edit' => __( 'Edit', 'foundationpress' ), /* Edit Dialog */
-				'edit_item' => __( 'Edit Event', 'foundationpress' ), /* Edit Display Title */
-				'new_item' => __( 'New Event', 'foundationpress' ), /* New Display Title */
-				'view_item' => __( 'View Event', 'foundationpress' ), /* View Display Title */
-				'search_items' => __( 'Search Events', 'foundationpress' ), /* Search Custom Type Title */
-				'not_found' =>  __( 'Nothing found in the Database.', 'foundationpress' ), /* This displays if there are no entries yet */
-				'not_found_in_trash' => __( 'Nothing found in Trash', 'foundationpress' ), /* This displays if there is nothing in the trash */
-				'parent_item_colon' => ''
+			array(
+				'labels' => array(
+					'name' => _x('events', 'Post Type General Name', 'foundationpress'),
+					'singular_name' => _x('events', 'Post Type Singular Name', 'foundationpress'),
+					'menu_name' => __('events', 'foundationpress'),
+					'name_admin_bar' => __('events posts', 'foundationpress'),
+					'archives' => __('Item Archives', 'foundationpress'),
+					'attributes' => __('Item Attributes', 'foundationpress'),
+					'parent_item_colon' => __('Parent Item:', 'foundationpress'),
+					'all_items' => __('All Items', 'foundationpress'),
+					'add_new_item' => __('Add New Item', 'foundationpress'),
+					'add_new' => __('Add New', 'foundationpress'),
+					'new_item' => __('New Item', 'foundationpress'),
+					'edit_item' => __('Edit Item', 'foundationpress'),
+					'update_item' => __('Update Item', 'foundationpress'),
+					'view_item' => __('View Item', 'foundationpress'),
+					'view_items' => __('View Items', 'foundationpress'),
+					'search_items' => __('Search Item', 'foundationpress'),
+					'not_found' => __('Not found', 'foundationpress'),
+					'not_found_in_trash' => __('Not found in Trash', 'foundationpress'),
+					'featured_image' => __('Featured Image', 'foundationpress'),
+					'set_featured_image' => __('Set featured image', 'foundationpress'),
+					'remove_featured_image' => __('Remove featured image', 'foundationpress'),
+					'use_featured_image' => __('Use as featured image', 'foundationpress'),
+					'insert_into_item' => __('Insert into item', 'foundationpress'),
+					'uploaded_to_this_item' => __('Uploaded to this item', 'foundationpress'),
+					'items_list' => __('Items list', 'foundationpress'),
+					'items_list_navigation' => __('Items list navigation', 'foundationpress'),
+					'filter_items_list' => __('Filter items list', 'foundationpress'),
 				), /* end of arrays */
-				'description' => __( 'This is the Events post type', 'foundationpress' ), /* Custom Type Description */
-				'public' => true,
-				'publicly_queryable' => false,
-				'exclude_from_search' => false,
-				'show_ui' => true,
-				'query_var' => true,
-				'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */
-				'menu_icon' => 'dashicons-money', /* the icon for the custom post type menu */
-				'rewrite'	=> array( 'slug' => 'event', 'with_front' => false ), /* you can specify its url slug */
-				'has_archive' => false, /* you can rename the slug here */
-				'capability_type' => 'post',
+				'label' => __('events', 'foundationpress'),
+				'description' => __('post for events sections', 'foundationpress'),
+
+				'supports' => array('title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats'),
+				'taxonomies' => array('category', 'post_tag'),
 				'hierarchical' => false,
-				/* the next one is important, it tells what's enabled in the post editor */
-				'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+				'public' => true,
+				'show_ui' => true,
+				'show_in_menu' => true,
+				'menu_position' => 5,
+				'menu_icon' => 'dashicons-location-alt',
+				'show_in_admin_bar' => true,
+				'show_in_nav_menus' => true,
+				'can_export' => true,
+				'has_archive' => true,
+				'exclude_from_search' => false,
+				'publicly_queryable' => true,
+				'capability_type' => 'post',
+				'show_in_rest' => true,
 			) /* end of options */
 		); /* end of register post type */
 
@@ -43,7 +63,7 @@
 	}
 
 		// adding the function to the Wordpress init
-		add_action( 'init', 'events_func');
+	add_action( 'init', 'events_func');
 
 	// now let's add custom categories (these act like categories)
 	register_taxonomy( 'events_cat',
