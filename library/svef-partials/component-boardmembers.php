b62bbb697eccac11ac8caeca9148d3195b126353
Birna Bryndís <thorkelsdottir@gmail.com>
2018-11-16 10:48:57 +0100

    boardmemebers-almost-there

---
+++ library/svef-partials/component-boardmembers.php
@@ -0,0 +1,123 @@
+<section class="section section--margin-bottom section--boardmembers ">
+    <div class="grid-container">
+        <div class="boardmember-cards grid-x grid-padding-x">
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image">
+                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member2.jpg' ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member2.gif' ?>)"></div>
+                        </div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell board-info medium-6 large-7">
+                <h2>Stjórnarmeðlimir SVEF árið 2018</h2>
+                <p>SVEF veit hve mikilvægt það fyrir fólkið í bransanum að vera vakandi fyrir nýjungum því í tækniheiminum eru breytingar örar og ný tækni...blablablabal eitthvað meir um það og því höfum við sett inn lestur vikunnar, getur verið bók, grein, video eða annað áhugavert. Ný tækni...blablablabal eitthvað meir um það og því höfum við sett inn lestur vikunnar, getur verið bók, grein, video eða annað áhugavert.</p>
+                <a href="#" >Skoða nánar</a>
+                <?php svef_partial('library/svef/icons/linkarrow.svg'); ?>
+            </div>
+
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member3.jpg' ?>)"></div>
+                        <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member3.gif' ?>)"></div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member1.jpg' ?>)"></div>
+                        <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member1.gif' ?>)"></div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image">
+                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member8.jpg' ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member8.gif' ?>)"></div>
+                        </div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image">
+                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member5.jpg' ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member5.gif' ?>)"></div>
+                        </div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image">
+                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member6.jpg' ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member6.gif' ?>)"></div>
+                        </div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-6 large-4">
+                <div class="card">
+                    <div class="member-image">
+                        <div class="member-image">
+                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member7.jpg' ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member7.gif' ?>)"></div>
+                        </div>
+                    </div>
+                    <div class="card-section">
+                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <div class="card-line"></div>
+                        <p >Stjórnarmeðlimur</p>
+                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	
+</section>
