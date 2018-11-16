c73a9dc0f7fc04fbe6d80b9a5a1389596ac002f1
Birna Bryndís <thorkelsdottir@gmail.com>
2018-11-16 17:16:55 +0100

    done with boardmember cards

--- library/boardmembers-custom-post-types.php
+++ library/boardmembers-custom-post-types.php
@@ -5,39 +5,39 @@
 			// let's now add all the options for this post type
 			array(
 				'labels' => array(
-					'name' => _x('boardmembers', 'Post Type General Name', 'foundationpress'),
+					'name' => _x('Boardmembers', 'Post Type General Name', 'foundationpress'),
 					'singular_name' => _x('Boardmembers', 'Post Type Singular Name', 'foundationpress'),
 					'menu_name' => __('Boardmembers', 'foundationpress'),
 					'name_admin_bar' => __('Boardmember posts', 'foundationpress'),
-					'archives' => __('Item Archives', 'foundationpress'),
-					'attributes' => __('Item Attributes', 'foundationpress'),
-					'parent_item_colon' => __('Parent Item:', 'foundationpress'),
-					'all_items' => __('All Items', 'foundationpress'),
-					'add_new_item' => __('Add New Item', 'foundationpress'),
+					'archives' => __('Boardmember Archives', 'foundationpress'),
+					'attributes' => __('Boardmember Attributes', 'foundationpress'),
+					'parent_item_colon' => __('Parent Boardmember:', 'foundationpress'),
+					'all_items' => __('All Boardmember', 'foundationpress'),
+					'add_new_item' => __('Add New Boardmember', 'foundationpress'),
 					'add_new' => __('Add New', 'foundationpress'),
-					'new_item' => __('New Item', 'foundationpress'),
-					'edit_item' => __('Edit Item', 'foundationpress'),
-					'update_item' => __('Update Item', 'foundationpress'),
-					'view_item' => __('View Item', 'foundationpress'),
-					'view_items' => __('View Items', 'foundationpress'),
-					'search_items' => __('Search Item', 'foundationpress'),
+					'new_item' => __('New Boardmember', 'foundationpress'),
+					'edit_item' => __('Edit Boardmember', 'foundationpress'),
+					'update_item' => __('Update Boardmember', 'foundationpress'),
+					'view_item' => __('View Boardmember', 'foundationpress'),
+					'view_items' => __('View Boardmembers', 'foundationpress'),
+					'search_items' => __('Search Boardmembers', 'foundationpress'),
 					'not_found' => __('Not found', 'foundationpress'),
 					'not_found_in_trash' => __('Not found in Trash', 'foundationpress'),
 					'featured_image' => __('Featured Image', 'foundationpress'),
 					'set_featured_image' => __('Set featured image', 'foundationpress'),
 					'remove_featured_image' => __('Remove featured image', 'foundationpress'),
 					'use_featured_image' => __('Use as featured image', 'foundationpress'),
-					'insert_into_item' => __('Insert into item', 'foundationpress'),
-					'uploaded_to_this_item' => __('Uploaded to this item', 'foundationpress'),
-					'items_list' => __('Items list', 'foundationpress'),
-					'items_list_navigation' => __('Items list navigation', 'foundationpress'),
-					'filter_items_list' => __('Filter items list', 'foundationpress'),
+					'insert_into_item' => __('Insert into Boardmember', 'foundationpress'),
+					'uploaded_to_this_item' => __('Uploaded to this Boardmember', 'foundationpress'),
+					'items_list' => __('Boardmembers list', 'foundationpress'),
+					'items_list_navigation' => __('Boardmembers list navigation', 'foundationpress'),
+					'filter_items_list' => __('Filter Boardmembers list', 'foundationpress'),
 				), /* end of arrays */
 				'label' => __('boardmembers', 'foundationpress'),
 				'description' => __('post for boardmembers sections', 'foundationpress'),
 
 				'supports' => array('title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats'),
-				'taxonomies' => array('category', 'post_tag'),
+				'taxonomies' => array('boardmembers_cat', 'boardmembers_year'),
 				'hierarchical' => false,
 				'public' => true,
 				'show_ui' => true,
@@ -80,6 +80,29 @@
 				'update_item' => __( 'Update Category', 'foundationpress' ), /* update title for taxonomy */
 				'add_new_item' => __( 'Add New Category', 'foundationpress' ), /* add new title for taxonomy */
 				'new_item_name' => __( 'New Category Name', 'foundationpress' ) /* name title for taxonomy */
+			),
+			'show_admin_column' => true,
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'type' ),
+		)
+	);
+
+	// now let's add custom categories (these act like categories)
+	register_taxonomy( 'boardmembers_year',
+		array('boardmembers'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
+		array('hierarchical' => true,     /* if this is true, it acts like categories */
+			'labels' => array(
+				'name' => __( 'Boardmember Year', 'foundationpress' ), /* name of the custom taxonomy */
+				'singular_name' => __( 'Boardmember Year', 'foundationpress' ), /* single taxonomy name */
+				'search_items' =>  __( 'Search Boardmember Years', 'foundationpress' ), /* search title for taxomony */
+				'all_items' => __( 'All Categories', 'foundationpress' ), /* all title for taxonomies */
+				'parent_item' => __( 'Parent Boardmember Year', 'foundationpress' ), /* parent title for taxonomy */
+				'parent_item_colon' => __( 'Parent Boardmember Year:', 'foundationpress' ), /* parent taxonomy title */
+				'edit_item' => __( 'Edit Boardmember Year', 'foundationpress' ), /* edit custom taxonomy title */
+				'update_item' => __( 'Update Boardmember Year', 'foundationpress' ), /* update title for taxonomy */
+				'add_new_item' => __( 'Add New Boardmember Year', 'foundationpress' ), /* add new title for taxonomy */
+				'new_item_name' => __( 'New Boardmember Year Name', 'foundationpress' ) /* name title for taxonomy */
 			),
 			'show_admin_column' => true,
 			'show_ui' => true,
--- library/svef-partials/component-boardmembers.php
+++ library/svef-partials/component-boardmembers.php
@@ -1,22 +1,60 @@
 <section class="section section--margin-bottom section--boardmembers bg-col-partial bg-col-partial--93--default">
     <div class="grid-container">
         <div class="boardmember-cards grid-x grid-padding-x">
+            <?php
+                $args = array (
+                    'post_type'       => 'boardmembers',
+                    'posts_per_page'	=>  1,
+                    'order'						=> 'ASC',
+                    'tax_query' => array(
+                        'relation' => 'AND',
+                        array(
+                            'taxonomy' => 'boardmembers_year',
+                            'field'    => 'slug',
+                            'terms' => '2017'
+                        ),
+                        array(
+                            'taxonomy' => 'boardmembers_cat',
+                            'field'    => 'slug',
+                            'terms' => 'board-director'
+                        )
+                    )
+
+                    );
+                    $the_query = new WP_Query( $args ); 
+            ?>
+            <?php
+                $boardmember_count = 0;
+                if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
+
+                    $boardmember_fullname = get_field('boardmember_fullname');
+                    $boardmember_role = get_field('boardmember_role');
+                    $boardmember_job_title = get_field('boardmember_job_title');
+                   
+                    $boardmember_image = get_field('boardmember_image');
+                    $use_gif_image = get_field('use_gif_image');
+                    $boardmember_gif_image = get_field('boardmember_gif_image');
+                    $boardmember_gif_image = $boardmember_gif_image ? $boardmember_gif_image : $boardmember_image;
+			?>
             <div class="cell medium-6 large-4">
                 <div class="card">
                     <div class="member-image">
                         <div class="member-image">
-                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member2.jpg' ?>)"></div>
-                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member2.gif' ?>)"></div>
+                            <div class="member-image-jpg" style="background-image: url(<?php echo $boardmember_image['sizes']['large'] ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo $boardmember_gif_image['sizes']['large'] ?>)"></div>
                         </div>
                     </div>
                     <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <p class="text--cardsmall"><?php echo $boardmember_fullname; ?></p>
                         <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                        <p><?php echo $boardmember_role; ?></p>
+                        <p class="text--small"><?php echo $boardmember_job_title; ?></p>
                     </div>
                 </div>
             </div>
+            
+            <?php endwhile; endif; wp_reset_query(); ?>
+
             <div class="cell board-info medium-6 large-7">
                 <h2>Stjórnarmeðlimir SVEF árið 2018</h2>
                 <p class="board-info-text">SVEF veit hve mikilvægt það fyrir fólkið í bransanum að vera vakandi fyrir nýjungum því í tækniheiminum eru breytingar örar og ný tækni...blablablabal eitthvað meir um það og því höfum við sett inn lestur vikunnar, getur verið bók, grein, video eða annað áhugavert. Ný tækni...blablablabal eitthvað meir um það og því höfum við sett inn lestur vikunnar, getur verið bók, grein, video eða annað áhugavert.</p>
@@ -24,98 +62,65 @@
                 <?php svef_partial('library/svef/icons/linkarrow.svg'); ?>
             </div>
 
-            <div class="cell medium-6 large-4">
-                <div class="card">
-                    <div class="member-image">
-                        <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member3.jpg' ?>)"></div>
-                        <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member3.gif' ?>)"></div>
-                    </div>
-                    <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
-                        <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cell medium-6 large-4">
-                <div class="card">
-                    <div class="member-image">
-                        <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member1.jpg' ?>)"></div>
-                        <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member1.gif' ?>)"></div>
-                    </div>
-                    <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
-                        <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cell medium-6 large-4">
-                <div class="card">
-                    <div class="member-image">
-                        <div class="member-image">
-                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member8.jpg' ?>)"></div>
-                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member8.gif' ?>)"></div>
-                        </div>
-                    </div>
-                    <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
-                        <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
-                    </div>
-                </div>
-            </div>
+                <?php
+                // var_dump(get_term_by( 'slug', 'board-director', 'boardmembers_cat'));
+                // $termObject = get_term_by( 'slug', 'board-director', 'boardmembers_cat');
+                // $board_cat_id = $termObject->term_id;
+                $args2 = array (
+                    'post_type'       => 'boardmembers',
+                    'posts_per_page'	=>  6,
+                    'order'						=> 'ASC',
+                    'tax_query' => array(
+                        'relation' => 'AND',
+                        array(
+                            'taxonomy' => 'boardmembers_year',
+                            'field'    => 'slug',
+                            'terms' => '2017'
+                        ), 
+                        array(
+                            'taxonomy' => 'boardmembers_cat',
+                            'field'    => 'slug',
+                            'terms'    => 'board-director',
+                            'operator' => 'NOT IN'
+                        )
+                    )
+
+                    );
+                    $the_second_query = new WP_Query( $args2 ); 
+            ?>
+            <?php
+                $boardmember_count = 0;
+                if ($the_second_query->have_posts()) : while ($the_second_query->have_posts()) : $the_second_query->the_post();
+
+                    $boardmember_fullname = get_field('boardmember_fullname');
+                    $boardmember_role = get_field('boardmember_role');
+                    $boardmember_job_title = get_field('boardmember_job_title');
+                   
+                    $boardmember_image = get_field('boardmember_image');
+                    $use_gif_image = get_field('use_gif_image');
+                    $boardmember_gif_image = get_field('boardmember_gif_image');
+                    $boardmember_gif_image = $boardmember_gif_image ? $boardmember_gif_image : $boardmember_image;
+			?>
             <div class="cell medium-6 large-4">
                 <div class="card">
                     <div class="member-image">
                         <div class="member-image">
-                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member5.jpg' ?>)"></div>
-                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member5.gif' ?>)"></div>
+                            <div class="member-image-jpg" style="background-image: url(<?php echo $boardmember_image['sizes']['large'] ?>)"></div>
+                            <div class="member-image-gif" style="background-image: url(<?php echo $boardmember_gif_image['sizes']['large'] ?>)"></div>
                         </div>
                     </div>
                     <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
+                        <p class="text--cardsmall"><?php echo $boardmember_fullname; ?></p>
                         <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
+                        <p><?php echo $boardmember_role; ?></p>
+                        <p class="text--small"><?php echo $boardmember_job_title; ?></p>
                     </div>
                 </div>
             </div>
-            <div class="cell medium-6 large-4">
-                <div class="card">
-                    <div class="member-image">
-                        <div class="member-image">
-                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member6.jpg' ?>)"></div>
-                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member6.gif' ?>)"></div>
-                        </div>
-                    </div>
-                    <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
-                        <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cell medium-6 large-4">
-                <div class="card">
-                    <div class="member-image">
-                        <div class="member-image">
-                            <div class="member-image-jpg" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member7.jpg' ?>)"></div>
-                            <div class="member-image-gif" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/assets/images/member7.gif' ?>)"></div>
-                        </div>
-                    </div>
-                    <div class="card-section">
-                        <p class="text--cardsmall">Ragnheiður Þorleifsdóttir</p>
-                        <div class="card-line"></div>
-                        <p >Stjórnarmeðlimur</p>
-                        <p class="text--small">Framkvæmdastjóri, Hugsmiðjan</p>
-                    </div>
-                </div>
-            </div>
+            
+            <?php endwhile; endif;  wp_reset_query(); ?>
+
+            
         </div>
     </div>
 
