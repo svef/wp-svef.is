fb7ae697ff4f5329a480bb3fd22f408a3379fb13
JonTryggvi <jontryggvi@jontryggvi.is>
2018-11-23 21:29:15 +0100

    Icewebb sett up þarf að skoða content betur

--- library/svef-partials/component-hero.php
+++ library/svef-partials/component-hero.php
@@ -1,5 +1,8 @@
-<?php $sectionHeight = $is_slide_show ? 'style="min-height:800px"' : ''; ?>
-<section class="section section--margin-bottom section--hero2" <?php echo $sectionHeight; ?>>
+<?php
+	$sectionHeight = $is_slide_show ? 'style="min-height:70vh"' : '';
+	$arrow_class = $hero_background_color == 'default' ? 'link-arrow' : 'link-arrow link-arrow--white'
+?>
+<section class="section section--margin-bottom section--hero" <?php echo $sectionHeight; ?>>
 	<div class="section__bg-fix section__bg-fix--<? echo $hero_background_color['value'];?>" aria-hidden="true"></div>
 	<div class="hero grid-container" >
 		<div class="grid-x">
@@ -25,20 +28,22 @@
 						<div class="cell small-12 medium-5 large-5 grid-x ">
 							<div class="section__info cell small-10 small-offset-1 medium-10 medium-offset-1 large-10 large-offset-1 top-text-padding">
 								<div class="">
-									<h2 class="section__title"><?php echo $hero_slider_title; ?></h2>
-									<p class="section__paragraphTall small-10 small-offset-1 medium-offset-0 large-offset-0"><?php echo $hero_slider_text; ?></p>
+									<h2 class="section__title bg-color--<? echo $hero_background_color['value'];?>"><?php echo $hero_slider_title; ?></h2>
+									<p class="section__paragraphTall bg-color--<? echo $hero_background_color['value'];?> small-10 small-offset-1 medium-offset-0 large-offset-0"><?php echo $hero_slider_text; ?></p>
 								</div>
+								<?php if($hero_link) : ?>
 								<div class="section__link section__link--hero cell small-5 small-offset-1 medium-5 medium-offset-0 large-5 large-offset-0">
-									<a href="<?php echo $hero_slider_link['url'] ?>" target="<?php echo $hero_slider_link['target'] ?>"><?php echo pll__('Lesa grein'); ?></a>
-									<?php svef_partial('library/svef/icons/linkarrow.svg'); ?>
+									<a href="<?php echo $hero_slider_link['url'] ?>" target="<?php echo $hero_slider_link['target'] ?>" class="bg-color--<? echo $hero_background_color['value'];?>"><?php echo pll__('Lesa grein'); ?></a>
+									<?php svef_partial('library/svef/icons/linkarrow.svg', array('link_arrow' => $arrow_class)); ?>
 								</div>
+								<?php endif; ?>
 							</div>
 						</div>
 				</div>
-</div>
+			</div>
 
 				<?php endwhile; ?>
-			</div>
+		</div>
 
 		<?php else : ?>
 		<div class="grid-x">
@@ -49,13 +54,15 @@
 			<div class="cell small-12 medium-5 large-5 grid-x ">
 				<div class="section__info cell small-10 small-offset-1 medium-10 medium-offset-1 large-10 large-offset-1 top-text-padding">
 
-						<h2 class="section__title"><?php echo $hero_title; ?></h2>
-						<p class="section__paragraphTall small-10 small-offset-1 medium-offset-0 large-offset-0"><?php echo $hero_text; ?></p>
+						<h2 class="section__title bg-color--<? echo $hero_background_color['value'];?>"><?php echo $hero_title; ?></h2>
+						<p class="section__paragraphTall bg-color--<? echo $hero_background_color['value'];?> small-10 small-offset-1 medium-offset-0 large-offset-0"><?php echo $hero_text; ?></p>
 
+					<?php if($hero_link) : ?>
 					<div class="section__link section__link--hero cell small-5 small-offset-1 medium-5 medium-offset-0 large-5 large-offset-0">
-					<a href="<?php echo $hero_link['url'] ?>" target="<?php echo $hero_link['target'] ?>"><?php echo pll__('Lesa grein'); ?></a>
-						<?php svef_partial('library/svef/icons/linkarrow.svg'); ?>
+						<a href="<?php echo $hero_link['url'] ?>" target="<?php echo $hero_link['target'] ?>" class="bg-color--<? echo $hero_background_color['value'];?>"><?php echo pll__('Lesa grein'); ?></a>
+						<?php svef_partial('library/svef/icons/linkarrow.svg', array('link_arrow' => $arrow_class)); ?>
 					</div>
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
--- library/svef-partials/component-logowall.php
+++ library/svef-partials/component-logowall.php
@@ -4,66 +4,66 @@
             <div class="grid-x grid-padding-x">
                 <h2 class="section__title cell medium-8 medium-offset-1 large-10 large-offset-1">Styrktaraðilar SVEF</h2>
                 <p class="section__paragraph cell medium-8 medium-offset-2 large-8 large-offset-2">SVEF á sér marga góða styrktaraðila sem eitthvað um það meira. Það er ein af blabla ástæðum þess að við getum haldið blabla og blal. Eitthvað meira mögulega og enn meira. Vera með! email linkur</p>
-                </div>  
+                </div>
             </div>
         </div>
-        
+
     </div>
-    <div class="grid-container">  
+    <div class="grid-container">
         <div class="supportors-logos" >
             <div class="logos-all grid-x grid-padding-x medium-8 medium-offset-2 cell large-10 large-offset-2">
-                <div class="logo-each logo-each cell medium-6 large-4"> 
+                <div class="logo-each logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo1.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo1.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo2.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo2.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo3.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo3.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo4.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo4.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo5.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo5.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo6.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo6.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo7.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo7.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo8.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo8.svg", $classArray );
                     ?>
                 </div>
                 <div class="logo-each cell medium-6 large-4">
                     <?php
                         $classArray = array("companyLogo" => "icon--dark40");
-                        svef_partial("library/svef/logos/logo9.svg", $classArray ); 
+                        svef_partial("library/svef/logos/logo9.svg", $classArray );
                     ?>
                 </div>
             </div>
