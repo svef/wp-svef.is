550ff27f122239a2142d6d3e99179f20f1ad4ee0
Birna Bryndís <thorkelsdottir@gmail.com>
2018-11-23 08:58:45 +0100

    logowall

---
+++ library/svef-partials/component-logowall.php
@@ -0,0 +1,72 @@
+<section class="section section--margin-bottom section--logowall">
+    <div class="grid-container">
+        <div class="introtext" >
+            <div class="grid-x grid-padding-x">
+                <h2 class="section__title cell medium-8 medium-offset-1 large-10 large-offset-1">Styrktaraðilar SVEF</h2>
+                <p class="section__paragraph cell medium-8 medium-offset-2 large-8 large-offset-2">SVEF á sér marga góða styrktaraðila sem eitthvað um það meira. Það er ein af blabla ástæðum þess að við getum haldið blabla og blal. Eitthvað meira mögulega og enn meira. Vera með! email linkur</p>
+                </div>  
+            </div>
+        </div>
+        
+    </div>
+    <div class="grid-container">  
+        <div class="supportors-logos" >
+            <div class="logos-all grid-x grid-padding-x medium-8 medium-offset-2 cell large-10 large-offset-2">
+                <div class="logo-each logo-each cell medium-6 large-4"> 
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo1.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo2.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo3.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo4.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo5.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo6.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo7.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo8.svg", $classArray ); 
+                    ?>
+                </div>
+                <div class="logo-each cell medium-6 large-4">
+                    <?php
+                        $classArray = array("companyLogo" => "icon--dark40");
+                        svef_partial("library/svef/logos/logo9.svg", $classArray ); 
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
