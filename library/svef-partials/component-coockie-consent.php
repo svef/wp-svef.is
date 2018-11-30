287f8652e18a7964806969284ec14fc831e7009d
JonTryggvi <jontryggvi@jontryggvi.is>
2018-11-30 15:57:30 +0100

    coockie consent flipi kominn

---
+++ library/svef-partials/component-coockie-consent.php
@@ -0,0 +1,12 @@
+
+<section class="section__cookie">
+	<div class="section__popup grid-container-fluid">
+		<div class="grid-x grid-padding-x">
+			<div class="section__cookie--message section__bg-color--dark cell small-auto small-offset-0 medium-10 medium-offset-1 grid-x">
+					<p class="small-12 medium-6"><?php pll_e( $cookie_message) ; ?> <br/> <a class="section__cookie--link" href="<?php echo $cookie_link; ?>"><?php pll_e('Vefstefna SVEF'); ?> </a></p>
+
+					<button id="btnCookieConsent" type="button" class="section__cookie--button btnMain cell small-4 medium-3"><?php pll_e('OK takk'); ?></button>
+			</div>
+		</div>
+	</div>
+</section>
