518a32d1176ac5334e79739479c2c29b88e53d57
JonTryggvi <jontryggvi@jontryggvi.is>
2018-12-06 22:09:41 +0100

    scrollmagic setup and tested on the statistics section

--- src/assets/js/partials/component-statistic.js
+++ src/assets/js/partials/component-statistic.js
@@ -1,11 +1,20 @@
-import Global from '../global-functions';
-
-
+import $ from 'jquery'
+import Global from '../global-functions'
+import ScrollMagic from 'scrollmagic/scrollmagic/minified/ScrollMagic.min';
+import 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min';
+import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min';
+// import TweenMax from 'gsap/src/minified/TweenMax.min';
+// import TimelineMax from 'gsap/src/minified/TimelineMax.min';
 const Statisticslider = {
 
   init() {
     this.cacheDom()
     this.addEvents()
+    $(this.humanSlider).exists(() => {
+      this.scrollSchene()
+
+    })
+
     document.documentElement.classList.add('js')
   },
   cacheDom() {
@@ -13,22 +22,22 @@ const Statisticslider = {
     this.humanSlider = this.body.querySelector('#humanSlider')
     this.humanSliderValue = this.body.querySelector('#humanSliderValue')
     this.humanSliderItems = this.body.querySelectorAll('.slider__humans--itemEach')
-    this._R = this.body.querySelector('[type=range]')
+    // this._R = this.body.querySelector('[type=range]')
   },
   addEvents() {
-    this.humanSlider.addEventListener('input', this.handleSliderChange.bind(this), false)
+    $(this.humanSlider).exists(() => {
+      this.humanSlider.addEventListener('input', this.handleSliderChange.bind(this), false)
+
+    })
   },
   handleSliderChange(e) {
     this.humanSliderValue.innerHTML = e.target.value;
-    Statisticslider._R.style.setProperty('--val', +Statisticslider._R.value)
+    this.humanSlider.style.setProperty('--val', +this.humanSlider.value)
     let minValue = e.target.min
     let maxValue = e.target.max
     let currentValue = e.target.value
     let iHumanItems = this.humanSliderItems.length
     let iHumanToPercentRatio = 100/iHumanItems
-
-
-
     let percent = Math.round(((currentValue - minValue) / (maxValue - minValue)) * 100);
     for (let i = 0; i < iHumanItems; i++) {
       if (Math.round(i * iHumanToPercentRatio) < percent) {
@@ -37,7 +46,37 @@ const Statisticslider = {
         this.humanSliderItems[i].classList.remove('slider__humans--itemEach--show')
       }
     }
-  }
+  },
+  scrollSchene() {
+    let controller = new ScrollMagic.Controller()
+    new ScrollMagic.Scene({
+      triggerElement: '#humanSlider',
+      duration: '50%',
+      offset: 0,
+      triggerHook: "onEnter",
+      // reverse: false
+    }).addIndicators({
+      name: 'slider',
+      colorTrigger: 'black',
+      colorStart: 'pink',
+      colorEnd: 'green'
+    }).addTo(controller).on('progress', this.scrollProgress.bind(this))
+  },
+  scrollProgress(e) {
+    this.humanSlider.style.setProperty('--val', +this.humanSlider.value)
+    this.humanSlider.value = Math.round((e.progress * 100) * 3.3333);
+    this.humanSliderValue.innerHTML = this.humanSlider.value
+    let iHumanItems = this.humanSliderItems.length
+    let iHumanToPercentRatio = 100/iHumanItems
+    let percent = Math.round((e.progress * 100))
+    for (let i = 0; i < iHumanItems; i++) {
+      if (Math.round(i * iHumanToPercentRatio) < percent) {
+        this.humanSliderItems[i].classList.add('slider__humans--itemEach--show')
+      } else {
+        this.humanSliderItems[i].classList.remove('slider__humans--itemEach--show')
+      }
+    }
+}
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   externals: {
-    jquery: 'jQuery'
+    jquery: 'jQuery',
   },
+
 };
