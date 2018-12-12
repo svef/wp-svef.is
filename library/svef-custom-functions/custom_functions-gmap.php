b7e1c742904af22a0cb449e891b258bc924e7ce1
JonTryggvi <jontryggvi@jontryggvi.is>
2018-12-12 14:56:52 +0100

    added some animations that are triggered after the initeal loader is done

--- library/svef-custom-functions/custom_functions-gmap.php
+++ library/svef-custom-functions/custom_functions-gmap.php
@@ -1,6 +1,12 @@
 <?php
 	function my_acf_init() {
-		acf_update_setting('google_api_key', 'AIzaSyAV31hWQmY3-HqsSQyKf7HxqMlCVoOwA0c');
+
+		acf_update_setting('google_api_key', 'AIzaSyDXmXhd0hbxYV8036AJqwgGdaDilwzhNFU');
 	}
+
 	add_action('acf/init', 'my_acf_init');
+
+	add_filter('acf/settings/google_api_key', function () {
+		return 'AIzaSyDXmXhd0hbxYV8036AJqwgGdaDilwzhNFU';
+	});
 ?>
