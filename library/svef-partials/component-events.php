c2f98f32c5f22bc07bcd8fdd995f8bbabbec3d90
JonTryggvi <jontryggvi@jontryggvi.is>
2018-12-16 14:56:44 +0100

    fix events links

--- library/svef-partials/component-events.php
+++ library/svef-partials/component-events.php
@@ -43,7 +43,8 @@
 					$location_name = get_field('event_location_name');
 					$location = get_field('event_location');
 
-					$link_is_external = $direct_link_isset ?  : get_permalink();
+					$link_is_external = $direct_link ?  : get_permalink();
+
 					$link_target = $direct_link_isset ? '_blank' : '' ;
 					$event_is_over = strtotime($event_date) < time() ? true : false;
 					$local_date = date_i18n("d M Y", strtotime($event_date));
